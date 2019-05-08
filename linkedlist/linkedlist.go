d03da08f19927cfa278e2c430817cafc68d30e6d
Joshua Herrera <herrjosh@oregonstate.edu>
2019-05-08 16:51:57 -0700

    restructure func to be called by LinkedList

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -9,4 +9,46 @@ type LinkedList struct {
 	Head *Node
 }
 
-//need to be able to addnode, delete node,
+//need to be able to addnode, delete node, add at beginning, delete at beginnign
+//delete some node, add after some node
+//all linkedlist functions should be called from head node
+/*func (head *Node) CreateNode() {
+	head.Data = 0
+	head.Next = nil
+}*/
+
+func (list *LinkedList) Add(v int) *LinkedList {
+	//create a temp node for the new value and a placeholder node
+	temp, p := new(Node)
+	temp.Data = v
+	//list is empty
+	if list.Head == nil {
+		list.Head = temp
+	} else {
+		p = list.Head
+		for p.Next != nil {
+			p = p.Next
+		}
+		p.Next = temp
+	}
+	return list
+}
+
+func (head *Node) AddAfter(n *Node, v int) *Node {
+	p, newN := new(Node)
+	newN.Data = v
+	if head == nil {
+		return head
+	}
+	p = head
+	for p.Next != nil {
+		if p == n {
+			newN.Next = n.Next
+			n.Next = newN
+		}
+		p = p.Next
+	}
+	return head
+}
+
+func (head *Node) AddStart(list *LinkedList)
