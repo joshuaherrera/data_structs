8191563874496351f5b93380a6f5ae9405f94d9a
Joshua Herrera <herrjosh@oregonstate.edu>
2019-05-08 16:03:19 -0700

    begin working on linkedllist implementation

---
+++ linkedlist/linkedlist.go
@@ -0,0 +1,12 @@
+package linkedlist
+
+type Node struct {
+	Data int
+	Next *Node
+}
+
+type LinkedList struct {
+	Head *Node
+}
+
+//need to be able to addnode, delete node,
--- stack/stack.go
+++ stack/stack.go
@@ -2,18 +2,21 @@ package stack
 
 type Stack struct {
 	Items []int
-	Top int
+	Top   int
 }
 
+//constant time amoritized.
 func (s *Stack) Push(v int) {
 	s.Items = append(s.Items, v)
 	s.Top = len(s.Items)
 }
 
+//constant time
 func (s *Stack) Peek() int {
 	return s.Items[s.Top-1]
 }
 
+//constant time
 func (s *Stack) Pop() int {
 	s.Top = s.Top - 1
 	v := s.Items[s.Top]
